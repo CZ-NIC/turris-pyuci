CZ-NIC/turris-pyuci

commit ebe462fc2a4891679c37b1c0ccda932871e5cad8

suppress deprecation warning
```
tests/euci_test.py::test_get_string
  /home/stepan/project/pyuci/euci/__init__.py:34: DeprecationWarning: Using or importing the ABCs from 'collections' instead of from 'collections.abc' is deprecated since Python 3.3, and in 3.9 it will stop working
    return isinstance(data, collections.Iterable) and not isinstance(data, str)
```

euci/__init__.py
@@ -24,6 +24,7 @@
 # EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 import collections
 import ipaddress
+
 from uci import Uci, UciExceptionNotFound
 from . import boolean
 
@@ -31,7 +32,7 @@ from . import boolean
 def _is_iter(data):
     """Check if data is instance of iterable with exclusion of string as the only standard iterable type we handle.
     """
-    return isinstance(data, collections.Iterable) and not isinstance(data, str)
+    return isinstance(data, collections.abc.Iterable) and not isinstance(data, str)
 
 
 class EUci(Uci):

commit 16070a1a31627b8a0bef682cca43144aaf2098ba

euci: fix missing import collections.abc
We have to import collections.abc instead of plain collectons as it does
not contain abc.
This fixes issue introduced by: ebe462fc2a4891679c37b1c0ccda932871e5cad8

euci/__init__.py
@@ -22,7 +22,7 @@
 # LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 # EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-import collections
+import collections.abc
 import ipaddress
 
 from uci import Uci, UciExceptionNotFound